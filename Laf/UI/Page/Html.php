9e8510c8ddc4d083e64cd5b169c003583fb29c46
Lulzim Avdiu <lulzimavdiu@gmail.com>
2020-02-05 00:23:32 +0200

    update on html page

--- Laf/UI/Page/Html.php
+++ Laf/UI/Page/Html.php
@@ -117,7 +117,6 @@ class Html
 	<meta charset='utf-8'>
 	<meta http-equiv='X-UA-Compatible' content='IE=edge'>
 	<meta name='viewport' content='width=device-width, initial-scale=1'>
-	<link rel='icon' href='data:;base64,='>
 	<title>{$this->getPageTitle()}</title>
 ";
 
