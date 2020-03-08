d2a9e75af84367902ca0bd7f630aad06686d95c2
Lulzim Avdiu <lulzimavdiu@gmail.com>
2020-03-09 00:08:53 +0200

    added modal class

--- Laf/UI/Container/Modal.php
+++ Laf/UI/Container/Modal.php
@@ -262,8 +262,8 @@ class Modal implements ComponentInterface
 
 		$html = "
 	<div class='modal fade' id='{$this->getId()}' tabindex='-1' role='dialog' aria-labelledby='{$this->getId()}Label' aria-hidden='true'>
-		<div class='modal-dialog bd-example-modal-lg' role='document'>
-			<div class='modal-content'>
+		<div class='modal-dialog' role='document'>
+			<div class='modal-content {$this->getSize()}'>
 				{$formStartTag}
 				<div class='modal-header'>
 					<h5 class='modal-title' id='{$this->getId()}Label}'>{$this->getTitle()}</h5>
