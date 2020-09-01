c6ce5e0e2adb394bcfff91df55acc108b975cfb5
Lulzim Avdiu <lulzimavdiu@gmail.com>
2020-08-31 21:05:43 -0400

    updated modal

--- Laf/UI/Container/Modal.php
+++ Laf/UI/Container/Modal.php
@@ -10,256 +10,280 @@ use Laf\UI\Traits\ComponentTrait;
 
 class Modal implements ComponentInterface
 {
-	use ComponentTrait;
+    use ComponentTrait;
 
-	/**
-	 * @var string
-	 */
-	protected $id = "";
+    /**
+     * @var string
+     */
+    protected $id = "";
 
-	/**
-	 * @var string
-	 */
-	protected $title = "";
+    /**
+     * @var bool
+     */
+    protected $formHasFiles = false;
 
-	/**
-	 * @var string
-	 */
-	protected $cancelLabel = "Cancel";
+    /**
+     * @var string
+     */
+    protected $title = "";
 
-	/**
-	 * @var string
-	 */
-	protected $size = "";
+    /**
+     * @var string
+     */
+    protected $cancelLabel = "Cancel";
 
-	/**
-	 * @var ComponentInterface[]
-	 */
-	protected $footerButtons = [];
+    /**
+     * @var string
+     */
+    protected $size = "";
 
-	/**
-	 * @var bool
-	 */
-	protected $includeForm = false;
+    /**
+     * @var ComponentInterface[]
+     */
+    protected $footerButtons = [];
 
-	/**
-	 * @var string
-	 */
-	protected $formAction = "";
+    /**
+     * @var bool
+     */
+    protected $includeForm = false;
 
-	/**
-	 * @var string
-	 */
-	protected $formMethod = "";
+    /**
+     * @var string
+     */
+    protected $formAction = "";
 
-	/**
-	 * @var string
-	 */
-	protected $formId = "";
+    /**
+     * @var string
+     */
+    protected $formMethod = "GET";
 
-
-	const SISZE_NORMAL = "";
-	const SIZE_LARGE = "modal-lg";
-
-
-	public function __construct(string $id)
-	{
-		$this->setId($id);
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getId(): string
-	{
-		return $this->id;
-	}
-
-	/**
-	 * @param string $id
-	 * @return Modal
-	 */
-	public function setId(string $id): Modal
-	{
-		$this->id = $id;
-		return $this;
-	}
-
-	/**
-	 * @param Link $button
-	 * @return Modal
-	 */
-	public function addFooterButton(Link $button): Modal
-	{
-		$this->footerButtons[] = $button;
-		return $this;
-	}
-
-	/**
-	 * @return array
-	 */
-	public function getFooterButtons(): array
-	{
-		return $this->footerButtons;
-	}
-
-	/**
-	 * @param array $buttons
-	 * @return Modal
-	 */
-	public function setFooterButtons(array $buttons = []): Modal
-	{
-		$this->footerButtons = $buttons;
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getTitle(): string
-	{
-		return $this->title;
-	}
-
-	/**
-	 * @param string $title
-	 * @return Modal
-	 */
-	public function setTitle(string $title): Modal
-	{
-		$this->title = $title;
-		return $this;
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getCancelLabel(): string
-	{
-		return $this->cancelLabel;
-	}
-
-	/**
-	 * @param string $cancelLabel
-	 * @return Modal
-	 */
-	public function setCancelLabel(string $cancelLabel): Modal
-	{
-		$this->cancelLabel = $cancelLabel;
-		return $this;
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getSize(): string
-	{
-		return $this->size;
-	}
-
-	/**
-	 * @param string $size
-	 * @return Modal
-	 */
-	public function setSize(string $size): Modal
-	{
-		$this->size = $size;
-		return $this;
-	}
-
-	/**
-	 * @return bool
-	 */
-	public function isIncludeForm(): bool
-	{
-		return $this->includeForm;
-	}
-
-	/**
-	 * @param bool $includeForm
-	 * @return Modal
-	 */
-	public function setIncludeForm(bool $includeForm): Modal
-	{
-		$this->includeForm = $includeForm;
-		return $this;
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getFormAction(): string
-	{
-		return $this->formAction;
-	}
-
-	/**
-	 * @param string $formAction
-	 * @return Modal
-	 */
-	public function setFormAction(string $formAction): Modal
-	{
-		$this->formAction = $formAction;
-		return $this;
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getFormMethod(): string
-	{
-		return $this->formMethod;
-	}
-
-	/**
-	 * @param string $formMethod
-	 * @return Modal
-	 */
-	public function setFormMethod(string $formMethod): Modal
-	{
-		$this->formMethod = $formMethod;
-		return $this;
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getFormId(): string
-	{
-		return $this->formId;
-	}
-
-	/**
-	 * @param string $formId
-	 * @return Modal
-	 */
-	public function setFormId(string $formId): Modal
-	{
-		$this->formId = $formId;
-		return $this;
-	}
+    /**
+     * @var string
+     */
+    protected $formId = "";
 
 
-	/**
-	 * @inheritDoc
-	 */
-	public function draw(): ?string
-	{
-		$formStartTag = "<form role='form' action='{$this->getFormAction()}' method='{$this->getFormMethod()}'>";
-		$formEndTag = "</form>";
-		$components = $footerButtons = "";
-		if (!$this->isIncludeForm()) {
-			$formStartTag = $formEndTag = "";
-		}
-		foreach ($this->getComponents() as $c) {
-			$components .= $c->draw();
-		}
-
-		foreach ($this->getFooterButtons() as $b) {
-			$footerButtons .= $b->draw();
-		}
+    const SISZE_NORMAL = "";
+    const SIZE_LARGE = "modal-lg";
 
 
-		$html = "
+    public function __construct(string $id)
+    {
+        $this->setId($id);
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     * @return Modal
+     */
+    public function setId(string $id): Modal
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @param Link $button
+     * @return Modal
+     */
+    public function addFooterButton(Link $button): Modal
+    {
+        $this->footerButtons[] = $button;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getFooterButtons(): array
+    {
+        return $this->footerButtons;
+    }
+
+    /**
+     * @param array $buttons
+     * @return Modal
+     */
+    public function setFooterButtons(array $buttons = []): Modal
+    {
+        $this->footerButtons = $buttons;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return Modal
+     */
+    public function setTitle(string $title): Modal
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCancelLabel(): string
+    {
+        return $this->cancelLabel;
+    }
+
+    /**
+     * @param string $cancelLabel
+     * @return Modal
+     */
+    public function setCancelLabel(string $cancelLabel): Modal
+    {
+        $this->cancelLabel = $cancelLabel;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSize(): string
+    {
+        return $this->size;
+    }
+
+    /**
+     * @param string $size
+     * @return Modal
+     */
+    public function setSize(string $size): Modal
+    {
+        $this->size = $size;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isIncludeForm(): bool
+    {
+        return $this->includeForm;
+    }
+
+    /**
+     * @param bool $includeForm
+     * @return Modal
+     */
+    public function setIncludeForm(bool $includeForm): Modal
+    {
+        $this->includeForm = $includeForm;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFormAction(): string
+    {
+        return $this->formAction;
+    }
+
+    /**
+     * @param string $formAction
+     * @return Modal
+     */
+    public function setFormAction(string $formAction): Modal
+    {
+        $this->formAction = $formAction;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFormMethod(): string
+    {
+        return $this->formMethod;
+    }
+
+    /**
+     * @param string $formMethod
+     * @return Modal
+     */
+    public function setFormMethod(string $formMethod): Modal
+    {
+        $this->formMethod = $formMethod;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFormId(): string
+    {
+        return $this->formId;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isFormHasFiles(): bool
+    {
+        return $this->formHasFiles;
+    }
+
+    /**
+     * @param bool $formHasFiles
+     * @return Modal
+     */
+    public function setFormHasFiles(bool $formHasFiles): Modal
+    {
+        $this->formHasFiles = $formHasFiles;
+        return $this;
+    }
+
+
+    /**
+     * @param string $formId
+     * @return Modal
+     */
+    public function setFormId(string $formId): Modal
+    {
+        $this->formId = $formId;
+        return $this;
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function draw(): ?string
+    {
+        $formStartTag = "<form role='form' action='{$this->getFormAction()}' method='{$this->getFormMethod()}' ".($this->isFormHasFiles()?" enctype='multipart/form-data' ":'').">";
+        $formEndTag = "</form>";
+        $components = $footerButtons = "";
+        if (!$this->isIncludeForm()) {
+            $formStartTag = $formEndTag = "";
+        }
+        foreach ($this->getComponents() as $c) {
+            $components .= $c->draw();
+        }
+
+        foreach ($this->getFooterButtons() as $b) {
+            $footerButtons .= $b->draw();
+        }
+
+
+        $html = "
 	<div class='modal fade' id='{$this->getId()}' tabindex='-1' role='dialog' aria-labelledby='{$this->getId()}Label' aria-hidden='true'>
 		<div class='modal-dialog {$this->getSize()}' role='document'>
 			<div class='modal-content'>
@@ -280,32 +304,32 @@ class Modal implements ComponentInterface
 		</div>
 	</div>
 ";
-		return $html;
+        return $html;
 
-	}
+    }
 
-	/**
-	 * Returns a button ready to open the modal
-	 * @param string $label
-	 * @param $cssClasses string classes to o assign to the button
-	 * @return Link
-	 */
-	public function getModalOpenbutton(string $label = "Open Modal", string $cssClasses = 'btn btn-outline-secondary btn-sm'): Link
-	{
-		$btn = new Link();
-		$btn->setValue('Open Modal')
-			->addAttribute('data-toggle', 'modal')
-			->addAttribute('data-target', '#' . $this->getId())
-			->setCssClasses(explode(' ', $cssClasses))
-			->addAttribute('href', 'javascript:;');
-		return $btn;
-	}
+    /**
+     * Returns a button ready to open the modal
+     * @param string $label
+     * @param $cssClasses string classes to o assign to the button
+     * @return Link
+     */
+    public function getModalOpenbutton(string $label = "Open Modal", string $cssClasses = 'btn btn-outline-secondary btn-sm'): Link
+    {
+        $btn = new Link();
+        $btn->setValue('Open Modal')
+            ->addAttribute('data-toggle', 'modal')
+            ->addAttribute('data-target', '#' . $this->getId())
+            ->setCssClasses(explode(' ', $cssClasses))
+            ->addAttribute('href', 'javascript:;');
+        return $btn;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getComponentCssControlClass(): string
-	{
-		return str_replace('\\', '-', static::class);
-	}
+    /**
+     * @return string
+     */
+    public function getComponentCssControlClass(): string
+    {
+        return str_replace('\\', '-', static::class);
+    }
 }
