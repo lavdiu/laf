418b1090476cc0a1b57c6c246a820b8544896d7e
Lulzim Avdiu <lulzimavdiu@gmail.com>
2021-01-29 16:29:27 +0200

    integrate PhpGrid in PageGenerator

--- Laf/Generator/PageGenerator.php
+++ Laf/Generator/PageGenerator.php
@@ -308,7 +308,8 @@ echo \$html->draw();
         $joins = [];
         foreach ($thisTable->getFields() as $field) {
             if ($field->isForeignKey()) {
-                $fkTable = new Table($thisTable->getForeignKey($field->getName()));
+                $fkClassName = '\\'.$this->getConfig()['namespace'].'\\'.$thisTable->getForeignKey($field->getName())->getTable()->getName();
+                $fkTable = new $fkClassName;
 
                 $columns[$thisTable->getName() . '_' . $field->getName()] = [$thisTable->getName(), $field->getName(), true];
                 $columns[$fkTable->getName() . '_' . $field->getName()] = [$fkTable->getName(), $field->getName(), false];
