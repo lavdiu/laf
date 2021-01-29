b4800c02eae64073e1fdc5393136cefa85ba2233
Lulzim Avdiu <lulzimavdiu@gmail.com>
2021-01-29 17:14:25 +0200

    integrate PhpGrid in PageGenerator

--- Laf/Generator/PageGenerator.php
+++ Laf/Generator/PageGenerator.php
@@ -311,8 +311,8 @@ echo \$html->draw();
                 $fkClassName = '\\' . $this->getConfig()['namespace'] . '\\' . $thisTable->getForeignKey($field->getName())->getReferencingTable();
                 $fkTable = (new $fkClassName)->getTable();
 
-                $columns[$thisTable->getName() . '_' . $field->getName()] = [$thisTable->getName(), $field->getName().'Id', false];
-                $columns[$fkTable->getName() . '_' . $fkTable->getDisplayField()->getName()] = ['id', $fkTable->getName(), true];
+                $columns[$thisTable->getName() . '_' . $field->getName()] = [$thisTable->getName(), $field->getName(), false];
+                $columns[$fkTable->getName() . '_' . $fkTable->getDisplayField()->getName()] = [$fkTable->getName(), $fkTable->getDisplayField()->getName(), true];
 
                 $joins[] = "LEFT JOIN `" . $fkTable->getName() . "` ON `" . $thisTable->getName() . '`.`' . $field->getName() . '` = `' . $fkTable->getName() . '`.`' . $thisTable->getForeignKey($field->getName())->getReferencingField() . '`';
             } else {
