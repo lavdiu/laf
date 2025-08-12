2ad31da5a0ba9eecbd412965ed3ee743979c6822
Lulzim Avdiu <lulzimavdiu@gmail.com>
2025-08-12 23:56:41 +0100

    refactor package and vendor location

--- src/Laf/Generator/DatabaseGenerator.php
+++ src/Laf/Generator/DatabaseGenerator.php
@@ -116,7 +116,7 @@ spl_autoload_register('{$this->getConfig()['namespace']}Autoloader');
 
 function {$this->getConfig()['namespace']}Autoloader(\$className)
 {
-	\$file = str_replace('\\\\', DIRECTORY_SEPARATOR, \$className);
+	\$file = str_replace('\\', DIRECTORY_SEPARATOR, \$className);
 	\$file = __DIR__ . '/../' . \$file . '.php';
 	#echo \"trying to include Class: {\$className}; file: {\$file}\\n<br />\";
 	if (file_exists(\$file) && is_readable(\$file)) {
