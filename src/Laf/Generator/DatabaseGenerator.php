e5bc18901493d048e2e7cf2238ac558c651e91dd
Lulzim Avdiu <lulzimavdiu@gmail.com>
2025-08-16 17:08:13 +0100

    Postgres compatibility issue with numeric fields

--- src/Laf/Generator/DatabaseGenerator.php
+++ src/Laf/Generator/DatabaseGenerator.php
@@ -166,7 +166,9 @@ function {$this->getConfig()['namespace']}Autoloader(\$className)
             SELECT table_name
             FROM information_schema.tables
             WHERE table_type = 'BASE TABLE'
-            AND table_schema NOT IN ('pg_catalog', 'information_schema');
+            AND table_schema NOT IN ('pg_catalog', 'information_schema')
+            ORDER BY table_name ASC
+            ;
             ";
         }
 
